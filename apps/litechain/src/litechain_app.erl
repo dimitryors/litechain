ddc1fc8a9b58ae4554963d321d04e5d100295c1d
root <root@ng-bsm.lab.croc.ru>
2017-08-15 19:15:33 +0300

    "add genesis block and ets"

--- apps/litechain/src/litechain_app.erl
+++ apps/litechain/src/litechain_app.erl
@@ -12,7 +12,7 @@
 %% Application callbacks
 -export([start_link/0, start/2, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([calcHash/4, nextBlock/1]).
+-export([new_block/1]).
 
 -define(SERVER, ?MODULE).
 -record(state, {name}).
@@ -29,6 +29,9 @@ start(_StartType, _StartArgs) ->
     litechain_sup:start_link().
 
 init([]) ->
+    litechain = ets:new(litechain,  [set, named_table]),
+    % Dev
+    add_block( genesisBlock() ),
     {ok, #state{}}.
 
 %%--------------------------------------------------------------------
@@ -38,11 +41,17 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+new_block(Data) -> gen_server:cast(litechain_app, {new_block, Data}).
+
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+handle_cast({new_block, Data}, State) ->
+    Rec = nextBlock(Data),
+    add_block(Rec),
+    {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -59,19 +68,34 @@ code_change(_OldVsn, State, _Extra) ->
 % Hash Block
 %-------------------------------------------------------------------------------
 calcHash(Index, Prehash, Timestamp, Data) ->
-    <<X:256/big-unsigned-integer>> = crypto:hash(sha256, Index ++ Prehash ++ Timestamp ++ Data),
+    IndexList       = integer_to_list(Index),
+    TimestampList   = integer_to_list(Timestamp),
+    Doc             = IndexList ++ Prehash ++ TimestampList ++ Data,
+    <<X:256/big-unsigned-integer>> = crypto:hash(sha256, Doc),
     integer_to_list(X, 16).
 
 %-------------------------------------------------------------------------------
 % Next Block
 %-------------------------------------------------------------------------------
 nextBlock(Data) ->
-    NewIndex,
-    PreHash,
-    NewTimestamp,
-    NewtHash = calcHash(NewIndex, PreHash, NewTimestamp, Data),
-    rec_block(NewIndex, Prehash, NewTimestamp, Data, NewtHash).
+    LastIndex           = ets:last(litechain),
+    [{_, LastBlock}]    = ets:lookup(litechain, LastIndex),
+    NewIndex            = LastBlock#block.index + 1,
+    PreHash             = LastBlock#block.hash,
+    NewTimestamp        = gregorian_seconds(),
+    NewHash             = calcHash(NewIndex, PreHash, NewTimestamp, Data),
+    rec_block(NewIndex, PreHash, NewTimestamp, Data, NewHash).
 
+%-------------------------------------------------------------------------------
+% Genesis Block
+%-------------------------------------------------------------------------------
+genesisBlock() ->
+    Index     = 1,
+    Prehash   = integer_to_list( gregorian_seconds() ),
+    Timestamp = gregorian_seconds(),
+    Data      = "genesis block",
+    Hash      = calcHash(Index, Prehash, Timestamp, Data),
+    rec_block(Index, Prehash, Timestamp, Data, Hash).
 
 %-------------------------------------------------------------------------------
 % Record from Clause for Block
@@ -83,3 +107,19 @@ rec_block(Index, Prehash, Timestamp, Data, Hash) ->
             data        = Data,
             hash        = Hash
 		    }.
+
+%-------------------------------------------------------------------------------
+% Get current timestamp in Gregorian seconds format
+%-------------------------------------------------------------------------------
+gregorian_seconds() ->
+    TS = os:timestamp(),
+    %calendar:gregorian_seconds_to_datetime(63670024974).
+    calendar:datetime_to_gregorian_seconds( calendar:now_to_universal_time(TS) ).
+
+%-------------------------------------------------------------------------------
+% Add block to litechain
+%-------------------------------------------------------------------------------
+add_block(Rec) when is_record(Rec, block) =:= true ->
+    ets:insert( litechain, { Rec#block.index, Rec#block{} } );
+add_block(Any) -> io:format("Any: ~n", Any).
+
